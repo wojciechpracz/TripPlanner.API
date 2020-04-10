38c13512238e9ac23338c01b9a1d7c919867441d
wojciechpracz <pracz.wojciech@gmail.com>
2020-04-10 13:37:52 +0200

    extended user repository

--- TripPlanner.API/Repository/UserRepository.cs
+++ TripPlanner.API/Repository/UserRepository.cs
@@ -21,5 +21,42 @@ namespace TripPlanner.API.Repository
             var users = await _context.Users.ToListAsync();
             return users;
         }
+
+        public async Task<User> GetUserAsync(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<int> UpdateUserAsync(User user)
+        {
+            var userFromContext = await _context.Users.FirstOrDefaultAsync(u => u.Id == user.Id);
+            
+            if (userFromContext != null)
+            {
+                userFromContext.FirstName = user.FirstName;
+                userFromContext.LastName = user.LastName; 
+                userFromContext.Email = user.Email;    
+            }
+
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> AddUserAsync(User user)
+        {           
+            await _context.Users.AddAsync(user);
+
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteUserAsync(int id)
+        {
+            var userFromContext = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            _context.Users.Remove(userFromContext);
+            return await _context.SaveChangesAsync();
+            
+        }
+
+
     }
 }
--- TripPlanner.Tests/RepositoryTests/UserRepositoryTests.cs
+++ TripPlanner.Tests/RepositoryTests/UserRepositoryTests.cs
@@ -40,6 +40,118 @@ namespace TripPlanner.Tests.RepositoryTests
                 //asert
                 Assert.Equal(3, result.Count());
             }
-        }        
+        }
+
+        [Fact]
+        public async void GetUserAsync_returns_correct_user()
+        {
+            //arange
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "GetUsersAsync_returns_correct_users")
+                .Options;
+
+            using(var context = new DataContext(options))
+            {
+                context.Users.Add(new User {FirstName = "Jan", LastName = "Kowalski", Email = "jan@gmail.com"});
+                context.Users.Add(new User {FirstName = "Henryk", LastName = "Kwiatkowski", Email = "Henryk@gmail.com"});
+                context.Users.Add(new User {FirstName = "Mirosław", LastName = "Nowak", Email = "Mirosław@gmail.com"});
+                context.SaveChanges();
+            }
+            
+            //act
+            using(var context = new DataContext(options)) 
+            {
+                var repo = new UserRepository(context);
+                var result = await repo.GetUserAsync(2);
+
+                //asert
+                Assert.Equal("Henryk", result.FirstName);
+                Assert.Equal("Kwiatkowski", result.LastName);
+                Assert.Equal("Henryk@gmail.com", result.Email);
+            }
+        }
+
+        [Fact]
+        public async void UpdateUserAsync_updates_user()
+        {
+            //arange
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "UpdateUserAsync_updates_user")
+                .Options;
+
+            using(var context = new DataContext(options))
+            {
+                context.Users.Add(new User {FirstName = "Jan", LastName = "Kowalski", Email = "jan@gmail.com"});
+                context.Users.Add(new User {FirstName = "Henryk", LastName = "Kwiatkowski", Email = "Henryk@gmail.com"});
+                context.Users.Add(new User {FirstName = "Mirosław", LastName = "Nowak", Email = "Mirosław@gmail.com"});                 
+                context.SaveChanges();
+             
+            }
+
+            using(var context = new DataContext(options)) 
+            {
+                var repo = new UserRepository(context);
+                var user = new User { Id = 2, FirstName = "Krzysztof", LastName = "Kwiatkowski", Email="Henryk@gmail.com"};
+
+                //act
+                var result = await repo.UpdateUserAsync(user);
+
+                //asert
+                Assert.Equal(1, result);
+            }
+       }
+
+       [Fact]
+       public async void AddUserAsync_adds_user()
+       {
+            //arange
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "AddUserAsync_adds_user")
+                .Options;
+
+            using(var context = new DataContext(options))
+            {
+                var repo = new UserRepository(context);
+                var user = new User { Id = 2, FirstName = "Krzysztof", LastName = "Kwiatkowski", Email="Henryk@gmail.com"};
+
+                //act
+                var result = await repo.AddUserAsync(user);
+
+                //asseert
+                Assert.Equal(1, result);
+            }
+       }
+
+       [Fact]
+       public async void DeleteUserAsync_deletesUser()
+       {
+            //arange
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "DeleteUserAsync_deletesUser")
+                .Options;
+
+            using(var context = new DataContext(options))
+            {
+                context.Users.Add(new User {FirstName = "Jan", LastName = "Kowalski", Email = "jan@gmail.com"});
+                context.Users.Add(new User {FirstName = "Henryk", LastName = "Kwiatkowski", Email = "Henryk@gmail.com"});
+                context.Users.Add(new User {FirstName = "Mirosław", LastName = "Nowak", Email = "Mirosław@gmail.com"});                 
+                context.SaveChanges();     
+            }
+
+            using(var context = new DataContext(options))
+            {
+                var userId = 2;
+                var repo = new UserRepository(context);
+
+                //act
+                repo.DeleteUserAsync(userId);
+
+                //assert
+                Assert.Equal(2, context.Users.Count());
+            }
+
+       }
+
+
     }
 }
